89df9a7ffc6edb19a8de61e43d34f3be7adb3587
David Benseman <DBenseman@aquinas.school.nz>
2024-05-15 09:08:12 +1200

    Initial Commit

---
+++ app.py
@@ -0,0 +1,47 @@
+""" Game information """
+import sqlite3
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+DATABASE = "game_data.db"
+
+
+def create_connection(db_file):
+    """ Create a connection to the sql database
+    Parameters: 
+    db_file - The name of the file
+    Returns: A connection to the database
+    """
+    try:
+        connection = sqlite3.connect(db_file)
+        return connection
+    except sqlite3.Error as e:
+        print(e)
+    return None
+
+
+@app.route('/')
+def render_home():
+    """ display the home page """
+    return render_template("index.html")
+
+@app.route("/games")
+def render_games():
+    type = request.args.get('type')
+    sort = request.args.get('sort')
+    query = "SELECT * FROM game"
+    if sort is not None:
+        query = query + " ORDER BY " + sort
+    conn = create_connection(DATABASE)
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+    cur.execute(query)
+    games = cur.fetchall()
+    conn.close()
+    return render_template("games.html", games=games, type=type)
+
+
+if __name__ == '__main__':
+    app.run()
